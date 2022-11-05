537bf3533b807e7be0e96e49e44a9b3e5273aedf
Prabesh KC <kcprabase@gmail.com>
2022-11-05 17:34:54 -0500

    modified app.js

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
+app.use((req, res, next) => {
+    console.log("REC_", req, res);
+    next();
+});
+app.use((err, req, res, next) => {
+    console.error("ERR_", err);
+    next();
+});
+
 
 const server = app.listen(process.env.Port, () => {
     console.log(process.env.AppStartMsg, server.address().port);
