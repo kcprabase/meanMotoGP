de89e5d0fb5abc05ec3f58a2fa402ed5315aca49
Prabesh KC <kcprabase@gmail.com>
2022-11-08 17:10:41 -0600

    angular app modified added home, footer and navigation component. added custom css and image of miu logo

--- public/mean-motogp-app/src/app/app.module.ts
+++ public/mean-motogp-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavigationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
