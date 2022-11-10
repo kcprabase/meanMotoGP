7f6394ba45463461564ef9c0b9941cd311b1cb7d
Prabesh KC <kcprabase@gmail.com>
2022-11-10 00:43:12 -0600

    added delete feature in angular app in prev commit

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ let corsOptions = {
 app.use(cors(corsOptions));
 // app.use("/api", (req, res, next) => {
 //     console.log("i'm here", req.method);
-//     // res.header('Access-Control-Allow-Origin', '*');
 //     res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
 //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 //     next();
