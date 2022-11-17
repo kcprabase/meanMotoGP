645051bd688dabc934f2bbb73e5150de130148d4
Prabesh KC <kcprabase@gmail.com>
2022-11-17 00:03:16 -0600

    updated get one, delete one, moved updateFull and update partial to promise

--- api/controllers/races.controller.js
+++ api/controllers/races.controller.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const mongoose = require("mongoose");
 const Race = mongoose.model(process.env.RaceModel);
 
@@ -5,15 +6,23 @@ const _sendResponse = (res, response) => {
     res.status(parseInt(response.status)).json(response.message);
 }
 
+const _createResponse = (status, message) => {
+    return { status, message };
+}
 
-const _findRaceByIdAndCallBack2 = (req, res) => {
+
+const _findRaceByIdAndCallBack2 = (req) => {
     return new Promise((resolve, reject) => {
         const raceId = req.params.raceId;
         if (mongoose.isValidObjectId(raceId)) {
-            Race.findById(raceId).exec().then((race) => { resolve(race); }).catch(err => { reject(err); });
+            Race.findById(raceId).exec().then((race) => {
+                if (!race) {
+                    reject({ status: process.env.ResourceNotFoundStatusCode, message: process.env.RaceIdNotFound });
+                } else { resolve(race); }
+            }).catch(err => { reject(err); });
         } else {
-            _sendResponse(res, { status: process.env.BadRequestStatusCode, message: process.env.InvalidRaceIdMsg });
-            reject();
+            // _sendResponse(res, { status: process.env.BadRequestStatusCode, message: process.env.InvalidRaceIdMsg });
+            reject({ status: process.env.BadRequestStatusCode, message: process.env.InvalidRaceIdMsg });
         }
     });
 }
@@ -77,23 +86,17 @@ const getAll = (req, res) => {
 //     _findRaceByIdAndCallBack(req, res, _getOne);
 // }
 const getOne = (req, res) => {
-    // const _getOne = (req, res, err, race) => {
-    //     if (err) {
-    //         response.status = process.env.InternalServerErrorStatusCode;
-    //         response.message = err;
-    //     }
-    //     if (!race) {
-    //         response.status = process.env.ResourceNotFoundStatusCode;
-    //         response.message = process.env.RaceWithIdDoesnotExist;
-    //     }
-    //     _sendResponse(res, response);
-    // }
     let response = { status: process.env.OkStatusCode, message: {} }
-    _findRaceByIdAndCallBack2(req, res).then(race => {
+    _findRaceByIdAndCallBack2(req).then(race => {
         response.message = race;
     }).catch(err => {
-        response.status = process.env.InternalServerErrorStatusCode;
-        response.message = err;
+        console.log(err);
+        if (err.status) {
+            response = err;
+        } else {
+            response.status = process.env.InternalServerErrorStatusCode;
+            response.message = err;
+        }
     }).finally(() => {
         _sendResponse(res, response);
     });
@@ -115,24 +118,32 @@ const addOne = (req, res) => {
 };
 
 const deleteOne = (req, res) => {
-    const _deleteOne = (req, res, err, race) => {
-        let response = {
-            status: process.env.NoContentSuccessStatusCode,
-            message: race
-        };
-        if (err) {
+    const raceId = req.params.raceId;
+    let response = {
+        status: process.env.NoContentSuccessStatusCode,
+        message: {}
+    };
+    if (mongoose.isValidObjectId(raceId)) {
+        Race.findByIdAndDelete(raceId).exec().then((race) => {
+            console.log("race is: ", race);
+            if (!race) {
+                response = { status: process.env.ResourceNotFoundStatusCode, message: process.env.RaceIdNotFound };
+            } else {
+                response = {
+                    status: process.env.NoContentSuccessStatusCode,
+                    message: race
+                };
+            }
+        }).catch(err => {
             response.status = process.env.InternalServerErrorStatusCode;
             response.message = err;
-        }
-        if (!race) {
-            response.status = process.env.ResourceNotFoundStatusCode;
-            response.message = process.env.RaceWithIdDoesnotExist;
-        } else {
-            race.delete();
-        }
-        _sendResponse(res, response);
+        }).finally(() => {
+            _sendResponse(res, response);
+        });
+    } else {
+        _sendResponse(res, { status: process.env.BadRequestStatusCode, message: process.env.InvalidRaceIdMsg });
+
     }
-    _findRaceByIdAndCallBack(req, res, _deleteOne);
 }
 
 const _updateOneCallBack = (req, res, err, race, callBack) => {
@@ -172,8 +183,40 @@ const _fullUpdateOneCallBack = (req, res, err, race) => {
     _updateOneCallBack(req, res, err, race, _setFullUpdateObject);
 }
 
-const fullUpdate = (req, res,) => {
-    _findRaceByIdAndCallBack(req, res, _fullUpdateOneCallBack);
+const _updateOneCallBack2 = (req, res, race) => {
+    let response = {
+        status: process.env.NoContentSuccessStatusCode,
+        message: {}
+    };
+    race.save().then(updatedGame => {
+        console.log("update success");
+        response.message = updatedGame;
+    }).catch(err => {
+        console.log("update fail");
+        response.status = process.env.InternalServerErrorStatusCode;
+        response.message = err;
+    }).finally(() => {
+        _sendResponse(res, response);
+    });
+}
+
+const _fullUpdateOneCallBack2 = (req, res, race) => {
+    race.circuitName = req.body.circuitName;
+    race.season = req.body.season;
+    race.winner = req.body.winner;
+
+    _updateOneCallBack2(req, res, race);
+}
+
+const fullUpdate = (req, resp) => {
+    let response = _createResponse(process.env.NoContentSuccessStatusCode, {});
+    _findRaceByIdAndCallBack2(req).then(race => {
+        _fullUpdateOneCallBack2(req, resp, race);
+    }).catch(err => {
+        console.log("race not found", err)
+        response = err;
+        _sendResponse(resp, response);
+    });
 }
 
 const _partialUpdateOneCallBack = (req, res, err, race) => {
@@ -185,8 +228,23 @@ const _partialUpdateOneCallBack = (req, res, err, race) => {
     _updateOneCallBack(req, res, err, race, _setPartialUpdateObject);
 }
 
+const _partialUpdateOneCallBack2 = (req, res, race) => {
+    if (req.body.circuitName) race.circuitName = req.body.circuitName;
+    if (req.body.season) race.season = req.body.season;
+    if (req.body.winner) race.winner = req.body.winner;
+    _updateOneCallBack2(req, res, race);
+}
+
 const partialUpdate = (req, res) => {
-    _findRaceByIdAndCallBack(req, res, _partialUpdateOneCallBack);
+    // _findRaceByIdAndCallBack(req, res, _partialUpdateOneCallBack);
+    let response = _createResponse(process.env.NoContentSuccessStatusCode, {});
+    _findRaceByIdAndCallBack2(req).then(race => {
+        _partialUpdateOneCallBack2(req, res, race);
+    }).catch(err => {
+        console.log("race not found", err)
+        response = err;
+        _sendResponse(resp, response);
+    });
 }
 
 module.exports = { getAll, getOne, addOne, deleteOne, fullUpdate, partialUpdate };
