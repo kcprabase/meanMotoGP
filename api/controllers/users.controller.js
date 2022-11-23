1a6b9c036c4ab11941b07f481e5f13cba1e6d5ff
Prabesh KC <kcprabase@gmail.com>
2022-11-23 01:24:21 -0600

    removed lint code

--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -5,7 +5,15 @@ var jwt = require('jsonwebtoken');
 
 const User = mongoose.model(process.env.UserModel);
 
-
+const login = (req, res) => {
+    let response = { status: process.env.OkStatusCode };
+    _checkBodyParamsForLogin(req, response)
+        .then(() => _findOneUserByUsername(req.body.username))
+        .then(user => _comparePassword(req, user, response))
+        .then(user => _createTokenResponse(user, response))
+        .catch(error => utility.appLog(error))
+        .finally(() => utility.sendResponse(res, response));
+}
 
 const register = (req, res) => {
     let response = { status: process.env.OkStatusCode };
@@ -120,57 +128,6 @@ const _comparePassword = (req, user, response) => {
                 reject(process.env.Unauthorized);
             });
     });
-}
-
-const login = (req, res) => {
-    let response = { status: process.env.OkStatusCode };
-    _checkBodyParamsForLogin(req, response)
-        .then(() => _findOneUserByUsername(req.body.username))
-        .then(user => _comparePassword(req, user, response))
-        .then(user => _createTokenResponse(user, response))
-        .catch(error => utility.appLog(error))
-        .finally(() => utility.sendResponse(res, response));
-
-    // (new Promise((resolve, reject) => {
-    //     User.findOne({ username: req.body.username })
-    //         .then(user => {
-    //             if (!user) {
-    //                 utility.appLog("user not found. go away");
-    //                 response.status = process.env.UnauthorizedStatusCode;
-    //                 response.message = "Unauthorized"
-    //                 reject();
-    //             } else {
-    //                 resolve(user);
-    //             }
-    //         }).catch(error => {
-    //             utility.appLog("error is ", error);
-    //             response.status = process.env.InternalServerErrorStatusCode;
-    //             response.message = error;
-    //             reject(error);
-    //         })
-    // }))
-    //     .then(user => {
-    //         return new Promise((resolve, reject) => {
-    //             bcrypt.compare(req.body.password, user.password).then((match) => {
-    //                 if (match) {
-    //                     const token = _getToken(user);
-    //                     response.message = { token: token }
-    //                 } else {
-    //                     //password dont match
-    //                     response.status = process.env.UnauthorizedStatusCode;
-    //                     response.message = "Unauthorized"
-    //                 }
-    //                 resolve();
-    //             }).catch(error => {
-    //                 //error while compaing password
-    //                 response.status = process.env.InternalServerErrorStatusCode;
-    //                 response.message = "Error occured"
-    //                 reject(error);
-    //             });
-    //         })
-    //     })
-    //     .catch(error => utility.appLog(error))
-    //     .finally(() => utility.sendResponse(res, response));
 }
 
 const _getToken = (user) => {
