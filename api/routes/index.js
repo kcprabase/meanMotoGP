ecddccd58557118316c6966d7c5ccb9a7e928c31
Prabesh KC <kcprabase@gmail.com>
2022-11-23 18:41:23 -0600

    added races route to races.routes.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,25 +1,27 @@
 const express = require("express");
-const raceController = require("../controllers/races.controller");
-const teamController = require("../controllers/teams.controller");
+// const raceController = require("../controllers/races.controller");
+// const teamController = require("../controllers/teams.controller");
 const userController = require("../controllers/users.controller");
 const router = express.Router();
+const racesRoutes = require("./races.routes");
 
-router.route("/races")
-    .get(raceController.getAll)
-    .post(raceController.addOne);
-router.route("/races/:raceId")
-    .get(raceController.getOne)
-    .delete(raceController.deleteOne)
-    .put(raceController.fullUpdate)
-    .patch(raceController.partialUpdate);
-router.route("/races/:raceId/teams")
-    .get(teamController.getAll)
-    .post(teamController.addOne);
-router.route("/races/:raceId/teams/:teamId")
-    .get(teamController.getOne)
-    .delete(teamController.deleteOne)
-    .put(teamController.fullUpdate)
-    .patch(teamController.partialUpdate);
+router.use("/races", racesRoutes);
+// router.route("/races")
+//     .get(raceController.getAll)
+//     .post(raceController.addOne);
+// router.route("/races/:raceId")
+//     .get(raceController.getOne)
+//     .delete(raceController.deleteOne)
+//     .put(raceController.fullUpdate)
+//     .patch(raceController.partialUpdate);
+// router.route("/races/:raceId/teams")
+//     .get(teamController.getAll)
+//     .post(teamController.addOne);
+// router.route("/races/:raceId/teams/:teamId")
+//     .get(teamController.getOne)
+//     .delete(teamController.deleteOne)
+//     .put(teamController.fullUpdate)
+//     .patch(teamController.partialUpdate);
 
 router.route("/users/register")
     .post(userController.register);
