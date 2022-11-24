e8676b93df53ac98db564abd0e37df88240106a3
Prabesh KC <kcprabase@gmail.com>
2022-11-24 00:09:13 -0600

    replaced hardcode with env variables

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,13 +1,10 @@
 const express = require("express");
-// const raceController = require("../controllers/races.controller");
-// const teamController = require("../controllers/teams.controller");
-// const userController = require("../controllers/users.controller");
 const router = express.Router();
 const racesRoutes = require("./races.routes");
 const usersRoutes = require("./users.routes");
 
-router.use("/races", racesRoutes);
-router.use("/users", usersRoutes);
+router.use(process.env.RacesRoute, racesRoutes);
+router.use(process.env.UsersRoute, usersRoutes);
 
 module.exports = router;
 
